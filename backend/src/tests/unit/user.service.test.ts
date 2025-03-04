31506dc11fc7e3cba7e1cb0c3168a283e8c193ba
CarlosJ09 <carlos.ml.jimenezz@gmail.com>
2025-03-04 17:41:18 -0400

    Add get user test

--- backend/src/tests/unit/user.service.test.ts
+++ backend/src/tests/unit/user.service.test.ts
@@ -1,5 +1,27 @@
-import { updateUser } from "@/services/user";
+import { getUser, updateUser } from "@/services/user";
 import { prismaMock } from "@/tests/mocks/prisma";
+
+test("should get a user", async () => {
+  const user = {
+    id: "1",
+    name: "Rich Haines",
+    email: "hello@prisma.io",
+    password: "hashedpassword",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+  prismaMock.user.findUnique.mockResolvedValue(user);
+
+  await expect(getUser("1")).resolves.toEqual(
+    expect.objectContaining({
+      id: "1",
+      name: "Rich Haines",
+      email: "hello@prisma.io",
+      password: "hashedpassword",
+    })
+  );
+});
 
 test("should update a user's name", async () => {
   const oldUser = {
@@ -13,7 +35,7 @@ test("should update a user's name", async () => {
 
   const updatedUser = {
     ...oldUser,
-    name: "James Peterson",
+    name: "Carlos",
     updatedAt: new Date(),
   };
 
@@ -24,7 +46,7 @@ test("should update a user's name", async () => {
   await expect(updateUser("1", updateData)).resolves.toEqual(
     expect.objectContaining({
       id: "1",
-      name: "James Peterson",
+      name: "Carlos",
       email: "hello@prisma.io",
       password: "hashedpassword",
     })
